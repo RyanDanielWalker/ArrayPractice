5b4544050b849fd9083d82e41963eacecdc4027b
Ryan Walker <ryandanielwalker@gmail.com>
2021-03-29 11:58:51 -0700

    Add arrays and methods to order on webpage.
    Co-authored-by: Jeremy Banka <jeremybanka@gmail.com>
    Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,18 @@ $(document).ready(function () {
     const answer3 = $("#question3").val();
 
     const favoriteThings = [answer1, answer2, answer3]
-
     console.log('fav things:', favoriteThings)
 
+    const favoriteThingsReversed = [];
+    favoriteThingsReversed.push(favoriteThings[2])
+    favoriteThingsReversed.push(favoriteThings[1])
+    favoriteThingsReversed.push(favoriteThings[0])
+    console.log("fav thing1", favoriteThingsReversed)
+
+    $('main').append('<ol/>')
+    $('ol').append('<li id="favorite-thing1"/>')
+    $('#favorite-thing1').text(favoriteThingsReversed[0])
+    $('ol').append('<li id="favorite-thing2"/>')
+    $('#favorite-thing2').text(favoriteThingsReversed[1])
   })
 })
